6e7730cffe3ac9f3f27cf2c1a5da07e72399ea19
biell <biell@DESKTOP-A5UI6R4>
2023-08-18 06:09:40 -0300

    Adicionando sobrenome e data de nascimento

--- ProjetoEstudoJAVA/src/javaProjeto/executavel/Pessoa.java
+++ ProjetoEstudoJAVA/src/javaProjeto/executavel/Pessoa.java
@@ -1,11 +1,27 @@
 package javaProjeto.executavel;
 
+import java.util.Date;
+
 public class Pessoa {
 	
 	private String nome;
 	private int idade;
+	private String sobrenome;
+	private Date dataNascimento;
 	
 	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
 	public String getNome() {
 		return nome;
 	}
