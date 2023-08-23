fb2bb4e7fb5a47705a8795c00d97ec3fa3075bb6
biell <biell@DESKTOP-A5UI6R4>
2023-08-23 19:58:16 -0300

    alteração soma para double

--- ProjetoEstudoJAVA/src/javaProjeto/executavel/testesAulas/Soma.java
+++ ProjetoEstudoJAVA/src/javaProjeto/executavel/testesAulas/Soma.java
@@ -4,11 +4,11 @@ public class Soma {
 
 	public static void main(String[] args){
 		
-		int nota1 = 60;
-		int nota2 = 80;
-		int nota3 = 20;
-		int nota4 = 100;
-		int nota5 = 100;
+		double nota1 = 62;
+		double nota2 = 85;
+		double nota3 = 24;
+		double nota4 = 100;
+		double nota5 = 100;
 		System.out.println("Media das notas =" + ((nota1 + nota2 +nota3 + nota4 + nota5)/5));
 	}
 }
