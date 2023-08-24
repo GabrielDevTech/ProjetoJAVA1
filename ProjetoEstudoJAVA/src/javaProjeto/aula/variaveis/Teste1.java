461b36e989a62abd39381978b0e510ca592c5313
biell <biell@DESKTOP-A5UI6R4>
2023-08-24 05:34:28 -0300

    Incluindo exercicio de switch

--- ProjetoEstudoJAVA/src/javaProjeto/aula/variaveis/Teste1.java
+++ ProjetoEstudoJAVA/src/javaProjeto/aula/variaveis/Teste1.java
@@ -4,13 +4,13 @@ public class Teste1 {
 
 	public static void main(String[] args) {
 
-		double nota1 = 62;
-		double nota2 = 85;
-		double nota3 = 24;
-		double nota4 = 100;
+		double nota1 = 40;
+		double nota2 = 20;
+		double nota3 = 0;
+		double nota4 = 20;
 		double nota5 = 100;
 		double media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
-		String media2 =  media >=70 ? "Aprovado" : "Reprovado";
+		String aprovacao2 =  media >=70 ? "aprovado" : media >= 40 ? "de recuperação" : "reprovado" ;
 		System.out.println("Media das notas = " + media);
 		
 		if(media >= 70) {
@@ -21,7 +21,7 @@ public class Teste1 {
 			System.out.println("Aluno reprovado.");
 		}
 		
-		System.out.println("Segundo critério aluno " + media2.toLowerCase() + ".");
+		System.out.println("Segundo critério aluno está " + aprovacao2.toLowerCase() + ".");
 	
 	}
 }
---
+++ ProjetoEstudoJAVA/src/javaProjeto/aula/variaveis/Teste4Switch.java
@@ -0,0 +1,34 @@
+package javaProjeto.aula.variaveis;
+
+public class Teste4Switch {
+	
+	public static void main(String[] args) {
+		int opcao = 3;
+		
+		switch(opcao) {
+		
+		case 1:
+			System.out.println("Segunda-Feira");
+			break;
+		case 2:
+			System.out.println("Terça-feira");
+			break;
+		case 3:
+			System.out.println("Quarta-feira");
+			break;
+		case 4:
+			System.out.println("Quinta-feira");
+			break;
+		case 5:
+			System.out.println("Sexta-feira");
+			break;
+		case 6:
+			System.out.println("Sabado");
+			break;
+		case 7:
+			System.out.println("Sabado");
+			break;
+		
+		}
+	}
+}
