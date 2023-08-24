c8b90c1cf6b7aae6aa2e4259387eb23e43d99b8d
biell <biell@DESKTOP-A5UI6R4>
2023-08-24 07:52:12 -0300

    Adicionando caixa de confirmação par continuar na classe Teste8CarroPessoa.

--- ProjetoEstudoJAVA/src/javaProjeto/aula/variaveis/Teste8CarroPessoa.java
+++ ProjetoEstudoJAVA/src/javaProjeto/aula/variaveis/Teste8CarroPessoa.java
@@ -3,18 +3,21 @@ import javax.swing.JOptionPane;
 public class Teste8CarroPessoa {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Vamos fazer a divisão de carros por pessoa");
-		
-		String carro = JOptionPane.showInputDialog("Digite a quantidade de carros");
-		String pessoa = JOptionPane.showInputDialog("Digite a quantidade de carros");
-		
-		int carroNumero = Integer.parseInt(carro);
-		int pessoaNumero = Integer.parseInt(pessoa);
-		
-		int restoCarro = carroNumero % pessoaNumero;
-		int divisaoCarro = carroNumero / pessoaNumero; 
-		System.out.println("A quantidade de caro pra cada pessoa ficou de:"
-		+ divisaoCarro +"\nA quantidade que sobou foi: " + restoCarro);
+		int continuar = 0;
+		while(continuar == 0) {
+			System.out.println("Vamos fazer a divisão de carros por pessoa.");
+			
+			String carro = JOptionPane.showInputDialog("Digite a quantidade de carros: ");
+			String pessoa = JOptionPane.showInputDialog("Digite a quantidade de pessoas: ");
+			
+			int carroNumero = Integer.parseInt(carro);
+			int pessoaNumero = Integer.parseInt(pessoa);
+			
+			int restoCarro = carroNumero % pessoaNumero;
+			int divisaoCarro = carroNumero / pessoaNumero; 
+			System.out.println("A quantidade de caro pra cada pessoa ficou de:"
+			+ divisaoCarro +"\nA quantidade que sobou foi: " + restoCarro);
+			continuar = JOptionPane.showConfirmDialog(null, "deseja continuar");
+		}
 	}
 }
