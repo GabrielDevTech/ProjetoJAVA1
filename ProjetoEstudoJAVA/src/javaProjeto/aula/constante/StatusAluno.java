0974a88f6dc767e2db47d57b1686cc2705f97e00
biell <biell@DESKTOP-A5UI6R4>
2023-09-12 06:25:00 -0300

    adicionando classe e constantes static statusAluno

---
+++ ProjetoEstudoJAVA/src/javaProjeto/aula/constante/StatusAluno.java
@@ -0,0 +1,9 @@
+package javaProjeto.aula.constante;
+/*Classe e constantes estáticas*/
+public class StatusAluno {
+
+	public static String APROVADO = "Aluno foi aprovado";
+	public static String RECUPERACAO = "Aluno ficou de recuperação";
+	public static String REPROVADO = "Aluno foi reprovado";
+	
+}
